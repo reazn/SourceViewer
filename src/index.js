e9aec2566b3bd8ae72c710e004847924307b3a08
REAZN <reasonwhygamer@gmail.com>
2021-04-01 22:45:45 +0100

    action testing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
-// if (require("electron-squirrel-startup")) {
-//   app.quit();
-// }
-
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 1280,
